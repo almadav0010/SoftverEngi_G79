d9bd4d4161ce4ebf9b51e8bcd9c926327b330fe2
alicehamori <alicehamori@gmail.com>
2026-05-10 13:36:52 +0200

    handwave unsupported listener types

---
+++ src/main/java/com/data_management/inputs/WebsocketDataListener.java
@@ -0,0 +1,17 @@
+package com.data_management.inputs;
+
+import com.data_management.DataStorage;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Handles reading data from a websocket generated as output by this software.
+ */
+public class WebsocketDataListener implements DataListener {
+
+  @Override
+  public List<String[]> readData() throws IOException {
+    throw new UnsupportedOperationException("Not supported:(");
+  }
+}
