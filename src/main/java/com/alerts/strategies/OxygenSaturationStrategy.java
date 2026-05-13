b384bebd363f0cfbb54ae56da57c846365725d48
alicehamori <alicehamori@gmail.com>
2026-05-13 15:22:00 +0200

    ready for implementing alert logic

--- src/main/java/com/alerts/strategies/OxygenSaturationStrategy.java
+++ src/main/java/com/alerts/strategies/OxygenSaturationStrategy.java
@@ -1,10 +1,21 @@
 package com.alerts.strategies;
 
+import com.data_management.Patient;
 import com.data_management.PatientRecord;
 
+import java.util.Optional;
+
 public class OxygenSaturationStrategy implements AlertStrategy {
+
+  private long lastProcessedTimeStamp = Long.MIN_VALUE;
+
   @Override
-  public boolean checkAlert(PatientRecord patientRecord) {
-    return false;
+  public long lastProcessedTimeStamp() {
+    return 0;
+  }
+
+  @Override
+  public Optional<String> checkAlert(Patient patient) {
+    return Optional.empty();
   }
 }
