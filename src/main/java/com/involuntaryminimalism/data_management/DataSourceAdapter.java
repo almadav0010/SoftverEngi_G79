5ddcd26bfe59ab8851903b1ae29b99c56e378453
alicehamori <alicehamori@gmail.com>
2026-05-19 15:57:30 +0200

    fix data storage singleton violation

--- src/main/java/com/involuntaryminimalism/data_management/DataSourceAdapter.java
+++ src/main/java/com/involuntaryminimalism/data_management/DataSourceAdapter.java
@@ -16,35 +16,37 @@ public class DataSourceAdapter {
   /**
    * Reads from specified data listener and adapts it to be placed in the data storage.
    *
-   * @param dataStorage destination of the read data
-   * @throws IOException when there is a problem with reading input (depends on the DataListener)
    */
-  public void putDataToStorage(DataStorage dataStorage) throws IOException {
-    List<String[]> semiRawData = dataListener.readData();
+  public void putDataToStorage() {
+    try {
+      List<String[]> semiRawData = dataListener.readData();
 
-    // contract specified in DataListener:
-    //  a list string arrays encoding a record in this specific order:
-    //  [patient id], [timestamp], [label], [data]
-    // ---------------------------------------------------------------------
-    // 1. properly parse parts of the entry
-    // 2. store parsed data in DataStorage
-    for (String[] entry : semiRawData) {
-      int patientID;
-      long timestamp;
-      Label label; // aka the record type
-      double data; // aka the measurement value
+      // contract specified in DataListener:
+      //  a list string arrays encoding a record in this specific order:
+      //  [patient id], [timestamp], [label], [data]
+      // ---------------------------------------------------------------------
+      // 1. properly parse parts of the entry
+      // 2. store parsed data in DataStorage
+      for (String[] entry : semiRawData) {
+        int patientID;
+        long timestamp;
+        Label label; // aka the record type
+        double data; // aka the measurement value
 
-      patientID = Integer.parseInt(entry[0]);
-      timestamp = Long.parseLong(entry[1]);
-      label = Label.valueOf(entry[2]);
-      data = RawParser.numeric(entry[3]); // handles special formats like percentages
+        patientID = Integer.parseInt(entry[0]);
+        timestamp = Long.parseLong(entry[1]);
+        label = Label.valueOf(entry[2]);
+        data = RawParser.numeric(entry[3]); // handles special formats like percentages
 
-      // alerts logs are not records therefore skipped
-      if (label == Label.Alert) {
-        continue;
+        // alerts logs are not records therefore skipped
+        if (label == Label.Alert) {
+          continue;
+        }
+        // gives parsed data to the storage in a way it understands (doing the adapter thing)
+        DataStorage.getInstance().addPatientData(patientID, data, label, timestamp);
       }
-      // gives parsed data to the storage in a way it understands (doing the adapter thing)
-      dataStorage.addPatientData(patientID, data, label, timestamp);
+    } catch (IOException e) {
+      System.out.println("Error with reading data from data listener. I will not put anything into data storage!");
     }
   }
 }
