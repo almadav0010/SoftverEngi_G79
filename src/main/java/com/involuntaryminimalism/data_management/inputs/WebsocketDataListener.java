5c4e5b3c22720e8059631efbcc9566692effdfa0
alicehamori <alicehamori@gmail.com>
2026-05-19 15:31:43 +0200

    fix java's lacking of removeFirst()

--- src/main/java/com/involuntaryminimalism/data_management/inputs/WebsocketDataListener.java
+++ src/main/java/com/involuntaryminimalism/data_management/inputs/WebsocketDataListener.java
@@ -20,11 +20,11 @@ public class WebsocketDataListener extends WebSocketClient implements DataListen
   they are requested via the readData() method call, upon which the dataQueue is
   emptied. Only validated String[] are added to the queue.
   */
-  private final List<String[]> dataQueue = new LinkedList<>();
+  private final LinkedList<String[]> dataQueue = new LinkedList<>();
 
   /**
    * Reads a chunk of data (accumulated since previous call of this method, and outputs them in the
-   * commonly agreed format understood by {@link com.data_management.DataSourceAdapter} class.
+   * commonly agreed format understood by {@link com.involuntaryminimalism.data_management.DataSourceAdapter} class.
    *
    * @return A list of patient records represented as String arrays
    * @throws IOException when error in websocket connection
@@ -33,7 +33,7 @@ public class WebsocketDataListener extends WebSocketClient implements DataListen
   public List<String[]> readData() throws IOException {
     var out = new LinkedList<String[]>();
     while (!dataQueue.isEmpty()) {
-      out.add(dataQueue.removeFirst());
+      out.add(dataQueue.remove(0));
     }
     return out;
   }
