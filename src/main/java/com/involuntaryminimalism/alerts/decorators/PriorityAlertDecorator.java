8e9e52887ce03de0cd98ab9ffe6ddbca3b2e618a
alicehamori <alicehamori@gmail.com>
2026-05-22 12:30:17 +0200

    implement priority alert decorator

--- src/main/java/com/involuntaryminimalism/alerts/decorators/PriorityAlertDecorator.java
+++ src/main/java/com/involuntaryminimalism/alerts/decorators/PriorityAlertDecorator.java
@@ -3,7 +3,17 @@ package com.involuntaryminimalism.alerts.decorators;
 import com.involuntaryminimalism.alerts.Alert;
 
 public class PriorityAlertDecorator extends AlertDecorator {
-  public PriorityAlertDecorator(Alert wrappee) {
+  private final int priority;
+  public PriorityAlertDecorator(Alert wrappee, int priority) {
     super(wrappee);
+    if (priority < 0) {
+      throw new IllegalArgumentException("Alert priority must be non negative, but " + priority + " was given!");
+    }
+    this.priority = priority;
+  }
+
+  @Override
+  public String getCondition() {
+    return "[PRIORITY: " + priority + "]: " + super.getCondition();
   }
 }
